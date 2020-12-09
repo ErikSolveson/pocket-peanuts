641a1d802628007cc437f50b39c51447fd9552fb
Erik <erik.solveson@gmail.com>
2020-12-08 18:02:41 -0700

    added gallery component

---
+++ my-site/src/Gallery.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+class Gallery extends Component {
+    constructor(){
+        super();
+        this.state = {};
+    }
+
+        //this is where functions can go
+
+
+        render() {
+            return(
+                <div>Hello World</div>
+            );
+        }
+    }
+
+    export default Gallery;
